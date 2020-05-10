998406bcfcd4ddde43ac91c7b31bb7f4e81ffc7a
Nick Jiang <congtao.jiang@outlook.com>
2020-05-10 19:58:33 +0800

    Destructure function parameter

--- index.js
+++ index.js
@@ -14,3 +14,19 @@ console.log('Node.JS with ESlint setup');
 const sayHelloLinting = (fName) => {
   console.log(`Hello linting, ${fName}`);
 };
+
+// ================  Experiment ES6 object destructuring feature ==============
+/**
+ * Greeting to the name.
+ *
+ * Using destructuring function parameters,with default / optional parameter value,
+ * and renaming the parameter name
+ *
+ * @param {} object with the 'name' property
+ */
+function greet({ name: paraName = 'congtao' } = {}) {
+  console.log(`Hello ${paraName}`);
+}
+
+greet(); // call with default parameter {}.  name uses the default value 'congtao'
+greet({ name: 'Jiang' });
